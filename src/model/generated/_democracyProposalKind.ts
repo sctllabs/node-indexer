67898d6c3a354bcdbbddbe8d9c240b0551585fb0
asansyzb <greatalibek@gmail.com>
2023-03-06 16:58:48 +0600

    feat: democracy schema update

---
+++ src/model/generated/_democracyProposalKind.ts
@@ -0,0 +1,16 @@
+import {AddMember} from "./_addMember"
+import {RemoveMember} from "./_removeMember"
+import {Spend} from "./_spend"
+import {TransferToken} from "./_transferToken"
+
+export type DemocracyProposalKind = AddMember | RemoveMember | Spend | TransferToken
+
+export function fromJsonDemocracyProposalKind(json: any): DemocracyProposalKind {
+    switch(json?.isTypeOf) {
+        case 'AddMember': return new AddMember(undefined, json)
+        case 'RemoveMember': return new RemoveMember(undefined, json)
+        case 'Spend': return new Spend(undefined, json)
+        case 'TransferToken': return new TransferToken(undefined, json)
+        default: throw new TypeError('Unknown json object passed as DemocracyProposalKind')
+    }
+}
